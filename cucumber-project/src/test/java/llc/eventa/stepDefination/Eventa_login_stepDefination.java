da7e5eb780537e2f579fa43ee14b407d7fbec13b
MehranDono <chowdhury.m2r@gmail.com>
2023-04-06 03:01:50 -0400

    Updated both settings.jason file - moved feature file to resources - Updated runner class

--- cucumber-project/src/test/java/llc/eventa/stepDefination/Eventa_login_stepDefination.java
+++ cucumber-project/src/test/java/llc/eventa/stepDefination/Eventa_login_stepDefination.java
@@ -1,4 +1,4 @@
-package StepDefination;
+package llc.eventa.stepDefination;
 import org.openqa.selenium.WebDriver;
 
 import io.cucumber.java.Before;
@@ -28,30 +28,30 @@ public class Eventa_login_stepDefination {
 
 
 
-    /*@Before("I open chrome browser")
+    @Before("@openChrome")
         public void I_open_chrome_browser()
         {
             System.out.println("I have opened Chrome !!!!!!!!!!!!!");
             Homepage.LaunchBrowser("chrome");
             PageA.ManageBrowserUtilities();
-        } */
+        }  
          
     
-    @Given("I land on eventa landing page")
-	    public void I_land_on_eventa_lading_page() 
+    @Given("^I land on eventa landing page$")
+	    public void i_land_on_eventa_landing_page() 
         {
 		    System.out.println("######I land on eventa landing page#####");
 		    Homepage.NavigateToUrl("https://www.eventa.llc"); // Driver 001 [ Chrome ] - go to eventa.llc
 	    }
 
-    @When("I type in username and password")
-        public void I_type_in_username_and_password()
+    @When("^I type in username and password$")
+        public void i_type_in_username_and_password()
         {
             System.out.println("######Typing in username and password#####");
             Homepage.login();
         }
     @Then("I expect to see matching username")
-        public void I_excepct_to_see_matching_username()
+        public void i_excepct_to_see_matching_username()
         {
             System.out.println("######I expect to username as Techno#####");
             Homepage.validateProfileNameExist("Techno");
---
+++ cucumber-project/src/test/java/llc/eventa/testRunner/MyCucumberRunner.java
@@ -0,0 +1,15 @@
+package llc.eventa.testRunner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/java/llc/eventa/resources/eventa_login.feature", 
+                 glue = "llc.eventa.stepDefination", 
+                 dryRun = true)
+public class MyCucumberRunner {
+    // ...
+}
+
